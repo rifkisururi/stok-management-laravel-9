9508adeed14e52306d962bb4939100c9bb7f1fca
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-05-26 15:27:43 +0700

    seeder

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'owner',
+            'email' => 'owner@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'owner',
+        ]);
+    }
+}
