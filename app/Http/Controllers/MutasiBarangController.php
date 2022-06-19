38ef28821a6dc2215dce1a017937369e1b88c42b
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-06-19 11:27:45 +0700

    pdf

--- app/Http/Controllers/MutasiBarangController.php
+++ app/Http/Controllers/MutasiBarangController.php
@@ -161,7 +161,9 @@ class MutasiBarangController extends Controller
                     YEAR(tanggal) Tahun, 
                     sum(jumlah*harga) as pembelian 
                 from 
-                    mutasi_barang mb where category = 'Masuk'
+                    mutasi_barang mb 
+                    inner join barang b on mb.id_barang = b.id
+                where category = 'Masuk'
                 group by 
                     MONTH(tanggal), YEAR(tanggal)
             ) a left join 
@@ -171,7 +173,9 @@ class MutasiBarangController extends Controller
                     YEAR(tanggal) Tahun, 
                     sum(jumlah*harga) as penjualan 
                 from 
-                    mutasi_barang mb where category = 'Keluar'
+                    mutasi_barang mb 
+                    inner join barang b on mb.id_barang = b.id
+                where category = 'Keluar'
                 group by 
                     MONTH(tanggal), YEAR(tanggal)
             ) b on a.Bulan = b.Bulan and a.Tahun = b.Tahun
